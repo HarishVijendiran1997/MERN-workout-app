93a33fdfb15b1d450bc40f0920eb1405ae32a1e8
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-04 17:08:43 +0530

    add cors to backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 //importing the routes
 import workoutRoutes from "./routes/workouts.routes.js";
@@ -12,6 +13,8 @@ dotenv.config();
 
 //declaring the express app
 const app = express();
+
+app.use(cors({ origin: "http://localhost:3000" }));
 
 //defining the port
 const PORT = process.env.PORT || 5000;
