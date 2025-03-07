246b238eb21c4d947f6070da26f1e81577e41636
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-08 01:39:30 +0530

    add: global context for auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { WorkoutsContextProvider } from '../contexts/WorkoutContext.jsx'
+import { AuthContextProvider } from '../contexts/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-      <WorkoutsContextProvider>
-        <App />
-      </WorkoutsContextProvider>
+  // <>
+  <AuthContextProvider>
+    <WorkoutsContextProvider>
+      <App />
+    </WorkoutsContextProvider>
+  </AuthContextProvider>
   // </StrictMode>,
 )
