dfd0ca850ec2dc55e0d1746513dd8c5f57e2c1e9
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-08 01:04:14 +0530

    add: email & password validation (npm validator)

--- backend/models/userModel.models.js
+++ backend/models/userModel.models.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema(
     {
@@ -16,6 +17,23 @@ const userSchema = new mongoose.Schema(
 )
 
 userSchema.statics.signup = async function(email, password) {
+
+    if(!email ||!password){
+        throw new Error("Please provide email and password")
+    }
+
+    if(!validator.isEmail(email)){
+        throw new Error("Invalid email")
+    }
+
+    if(password.length < 8){
+        throw new Error("Password should be at least 8 characters long")
+    }
+
+    if(!validator.isStrongPassword(password)){
+        throw new Error("Password should be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+    }
+
     const exists = await this.findOne({ email})
 
     if(exists){
