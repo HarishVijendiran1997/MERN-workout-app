94a2de9bf69d8ad06b82180ad4caac129635b8cf
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-08 01:16:11 +0530

    add: generate signing jwt

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,14 +1,25 @@
 import { User } from "../models/userModel.models.js";
+import jwt from "jsonwebtoken";
 
+//? Generate JWT token
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
+//?Login user
 const loginUser = async (req, res) => {
   res.json({ message: "loginUser" });
 };
 
+//? Signup user
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ message: "User created successfully", email, user });
+    const token = createToken(user._id);
+    res
+      .status(200)
+      .json({ message: "User created successfully", email, token });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
