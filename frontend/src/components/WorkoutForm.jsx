9b7dc853c8e420632408e92afaf9984a1c28d41e
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-07 15:32:16 +0530

    fix: input fields empty highlights work in dark

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -17,8 +17,19 @@ const WorkoutForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        let missingFields = [];
+
+        if (!title.trim()) missingFields.push("title");
+        if (!load || load < 0) missingFields.push("load");
+        if (!reps || reps < 0) missingFields.push("reps");
+
+
+        if (missingFields.length > 0) {
+            setEmptyFields(missingFields);
+        }
+
         if (!title.trim() && !load && !reps) {
-            setError("All fields are required", emptyFields);
+            setError("Please fill in all required fields.");
             return;
         }
 
@@ -57,6 +68,7 @@ const WorkoutForm = () => {
         if (isSubmitting) return
         setIsSubmitting(true)
         setError(null)
+        setEmptyFields([]);
         try {
             const response = await axios.post("http://localhost:4000/api/workouts/", {
                 title,
@@ -105,19 +117,19 @@ const WorkoutForm = () => {
             <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-6 bg-white dark:bg-darkSecondary dark:text-darkInputText  shadow-md rounded-lg
 transition-colors duration-200">
                 <h2 className="text-2xl font-bold text-blue-600 dark:text-darkTextPrimary mb-4 transition-colors duration-200">Add a new workout</h2>
-                <input className={`w-full p-2 mb-4 border dark:border-darkBorder rounded-lg dark:bg-darkInputBackground transition-colors duration-200 ${emptyFields.includes('title') ? "border-red-500" : "border-gray-300"}`}
+                <input className={`w-full p-2 mb-4 border  rounded-lg dark:bg-darkInputBackground transition-colors duration-200 ${emptyFields.includes('title') ? "border-red-500 dark:border-red-500" : "border-gray-300 dark:border-darkBorder"}`}
                     type="text"
                     onChange={(e) => setTitle(e.target.value)}
                     value={title}
                     placeholder="Workout Title"
                 />
-                <input className={`w-full p-2 mb-4 border dark:border-darkBorder rounded-lg dark:bg-darkInputBackground transition-colors duration-200 ${emptyFields.includes('load') ? "border-red-500" : "border-gray-300"}`}
+                <input className={`w-full p-2 mb-4 border dark:border-darkBorder rounded-lg dark:bg-darkInputBackground transition-colors duration-200 ${emptyFields.includes('load') ? "border-red-500 dark:border-red-500" : "border-gray-300 dark:border-darkBorder"}`}
                     type="number"
                     onChange={handleLoadChange}
                     value={load}
                     placeholder="Load (kg)"
                 />
-                <input className={`w-full p-2 mb-4 border dark:border-darkBorder rounded-lg dark:bg-darkInputBackground transition-colors duration-200 ${emptyFields.includes('reps') ? "border-red-500" : "border-gray-300"}`}
+                <input className={`w-full p-2 mb-4 border dark:border-darkBorder rounded-lg dark:bg-darkInputBackground transition-colors duration-200 ${emptyFields.includes('reps') ? "border-red-500 dark:border-red-500" : "border-gray-300 dark:border-darkBorder"}`}
                     type="number"
                     onChange={handleRepsChange}
                     value={reps}
