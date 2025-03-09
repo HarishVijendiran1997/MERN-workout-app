f43e9c49787b289750984f7f3903cdb2192aa969
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-10 03:33:27 +0530

    fix: limit only one in progress status

--- frontend/src/components/WorkoutStatus.jsx
+++ frontend/src/components/WorkoutStatus.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { useWorkoutsContext } from "../../hooks/useWorkoutsContext";
 import { useAuthContext } from "../../hooks/useAuthContext";
@@ -7,20 +7,36 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 const WorkoutStatus = ({ workout }) => {
     const { dispatch } = useWorkoutsContext();
     const { user } = useAuthContext();
-    const [status, setStatus] = useState(workout.status);
 
     const handleWorkoutStatus = async (e) => {
         const newStatus = e.target.value;
-        setStatus(newStatus);
+        if (!user) {
+            toast.error("You must be logged in!");
+            return;
+        }
 
         try {
-            const response = await axios.patch(
+            const response = await axios.get("http://localhost:4000/api/workouts", {
+                headers: {
+                    Authorization: `Bearer ${user.token}`
+                }
+            });
+            const allWorkouts = response.data.workouts
+            const currentlyInProgress = allWorkouts.find(w => w.status === "in progress")
+            if (currentlyInProgress) {
+                await axios.patch(`http://localhost:4000/api/workouts/${currentlyInProgress._id}`,
+                    { status: "pending" },
+                    { headers: { Authorization: `Bearer ${user.token}` } }
+                );
+                dispatch({ type: "UPDATE_WORKOUT", payload: { ...currentlyInProgress, status: "pending" } });
+            }
+            const updateResponse = await axios.patch(
                 `http://localhost:4000/api/workouts/${workout._id}`,
                 { status: newStatus },
                 { headers: { Authorization: `Bearer ${user.token}` } }
             );
-            dispatch({ type: "UPDATE_WORKOUT", payload: response.data.updateWorkout });
-            toast.success("Workout status updated!");
+            dispatch({ type: "UPDATE_WORKOUT", payload: updateResponse.data.updateWorkout });
+            toast.success(`Workout status updated to ${newStatus}`);
         } catch (error) {
             toast.error("Failed to update status!");
         }
@@ -29,19 +45,19 @@ const WorkoutStatus = ({ workout }) => {
     return (
         <div className="flex space-x-2 translate-y-1">
             <div>
-                <p><strong className={`transition-colors duration-200 ${status === "completed" ? "text-neutral-800 dark:text-darkDisabledText cursor-not-allowed line-through" : "dark:text-darkTextSecondary"}`}>Status :</strong></p>
+                <p><strong className={`transition-colors duration-200 ${workout.status === "completed" ? "text-neutral-800 dark:text-darkDisabledText cursor-not-allowed line-through" : "dark:text-darkTextSecondary"}`}>Status :</strong></p>
             </div>
             <div>
                 <select
                     className={`flex text-center border border-gray-400 dark:border-darkBorder p-1 rounded-lg transition-colors duration-200 
-                        ${status === "completed" ? "bg-green-400 text-neutral-700 dark:bg-darkAddButtonHover dark:text-darkInputText" : status === "in progress" ? "bg-white text-neutral-800 dark:bg-darkTestButton dark:text-darkInputBackground " : "bg-white text-neutral-800 dark:bg-darkInputBackground dark:text-darkTextResult"}`}
-                    value={status}
+                        ${workout.status === "completed" ? "bg-green-400 text-neutral-700 dark:bg-darkAddButtonHover dark:text-darkInputText" : workout.status === "in progress" ? "bg-white text-neutral-800 dark:bg-darkTestButton dark:text-darkInputBackground " : "bg-white text-neutral-800 dark:bg-darkInputBackground dark:text-darkTextResult"}`}
+                    value={workout.status}
                     onChange={handleWorkoutStatus}
                 >
                     <option value="" disabled className={`${status === "completed" ? "bg-white  dark:bg-darkInputBackground" : status === "in progress" ? "bg-white  dark:bg-darkInputBackground" : ""}`}>Select Status</option>
-                    <option value="pending" className={`${status === "completed" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : status === "in progress" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : ""}`}>Pending</option>
-                    <option value="in progress" className={`${status === "completed" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : status === "in progress" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : ""}`}>On Progress</option>
-                    <option value="completed" className={`${status === "completed" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : status === "in progress" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : ""}`}>Completed</option>
+                    <option value="pending" className={`${workout.status === "completed" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : status === "in progress" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : ""}`}>Pending</option>
+                    <option value="in progress" className={`${workout.status === "completed" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : status === "in progress" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : ""}`}>On Progress</option>
+                    <option value="completed" className={`${workout.status === "completed" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : status === "in progress" ? "bg-white dark:text-darkTextResult dark:bg-darkInputBackground" : ""}`}>Completed</option>
                 </select>
             </div>
         </div>
