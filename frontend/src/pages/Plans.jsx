bae53b64e60e250f8bb785afca82edfcfb91534b
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-13 14:41:07 +0530

    add: back button to plans and profile pages

--- frontend/src/pages/Plans.jsx
+++ frontend/src/pages/Plans.jsx
@@ -4,8 +4,10 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useDowngrade } from "../../hooks/useDowngrade";
+import { useNavigate } from "react-router-dom";
 const Plans = () => {
     const { user } = useAuthContext()
+    const navigate = useNavigate()
     const { downgrade, downgradeError, downgradeIsLoading } = useDowngrade()
     const { upgrade, upgradeError, upgradeIsLoading } = useUpgrade()
 
@@ -36,12 +38,13 @@ const Plans = () => {
     }
 
     return (
-        <div className="w-full flex flex-col items-center justify-center bg-neutral-200 text-blue-900 dark:bg-darkPrimary dark:text-white">
-            <h1 className="text-3xl font-bold mb-8">Choose Your Plan</h1>
+        <div className="w-full flex flex-col items-center justify-center bg-neutral-200 text-blue-900 dark:bg-darkPrimary dark:text-white relative">
+            <button className="text-white absolute top-5 left-5 cursor-pointer bg-blue-900 hover:bg-blue-950 dark:bg-darkTertiary dark:hover:bg-darkSecondary rounded-lg p-2" onClick={()=>{navigate("/")}}>Back</button>
+            <h1 className="text-3xl font-bold mb-8 dark:text-darkTextPrimary">Choose Your Plan</h1>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 min-w-md sm:max-w-5xl gap-8">
                 {/* Basic Plan */}
-                <div className="bg-neutral-300 hover:scale-110 duration-500 ease-in-out transition-transform dark:bg-darkTertiary p-8 m-2 rounded-lg shadow-lg flex flex-col items-center">
+                <div className="bg-neutral-300 hover:scale-105 duration-500 ease-in-out transition-transform dark:bg-darkTertiary p-8 m-2 rounded-lg shadow-lg flex flex-col items-center">
                     <h2 className="text-2xl font-bold mb-2 text-neutral-800 dark:text-darkTextPrimary">Basic Plan</h2>
                     <p className="text-neutral-700 dark:text-darkTextResult">Access limited features</p>
                     <button onClick={handleDowngrade} disabled={downgradeIsLoading || user.plan === "Basic"} className={`px-5 py-2 mt-4 rounded-lg text-white transition-all duration-300 ${user?.plan === "Basic"
@@ -54,7 +57,7 @@ const Plans = () => {
                 </div>
 
                 {/* Premium Plan */}
-                <div className="bg-neutral-300 hover:scale-110 duration-500 ease-in-out transition-transform dark:bg-darkTertiary p-8 m-2 rounded-lg shadow-lg flex flex-col items-center">
+                <div className="bg-neutral-300 hover:scale-105 duration-500 ease-in-out transition-transform dark:bg-darkTertiary p-8 m-2 rounded-lg shadow-lg flex flex-col items-center">
                     <h2 className="text-2xl font-bold mb-2 text-neutral-800  dark:text-darkTextPrimary">Premium Plan</h2>
                     <p className="text-neutral-700 dark:text-darkTextResult">Unlock workout status feature</p>
                     <button
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { FaCrown } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
     const { user } = useAuthContext();
+    const navigate = useNavigate();
 
     return (
-        <div className="flex w-full flex-col items-center justify-center min-h-screen bg-neutral-200 dark:bg-darkPrimary text-blue-900 dark:text-white">
+        <div className="flex w-full flex-col items-center justify-center bg-neutral-200 dark:bg-darkPrimary text-blue-900 dark:text-darkTextPrimary relative">
+            <button className="text-white absolute top-5 left-5 cursor-pointer bg-blue-900 hover:bg-blue-950 dark:bg-darkTertiary dark:hover:bg-darkSecondary rounded-lg p-2" onClick={()=>{navigate("/")}}>Back</button>
             <h1 className="text-3xl font-bold">Profile Page</h1>
             <div className="sm:w-1/3 flex px-10">
                 {user &&
                     <div>
-                        <p className="text-lg mt-4">User Email: {user.email}</p>
+                        <p className="text-neutral-800 text-lg mt-4 dark:text-darkTextResult"><strong className="dark:text-darkTextSecondary text-blue-900">User Email: </strong>{user.email}</p>
                         <div className="flex gap-2">
-                            <p className="text-lg mt-4">User Plan: {user.plan}</p>{user.plan === "Premium" && (
+                            <p className="text-neutral-800 text-lg mt-4 dark:text-darkTextResult"><strong className="dark:text-darkTextSecondary text-blue-900">User Plan: </strong>{user.plan}</p>{user.plan === "Premium" && (
                                 <FaCrown size={20} color="#FFA500" className="translate-y-5" />
                             )}
                         </div>
-                        <p className="text-lg mt-4">User ID: {user._id}</p>
+                        <p className="text-neutral-800 text-lg mt-4 dark:text-darkTextResult"><strong className="dark:text-darkTextSecondary text-blue-900">User ID: </strong>{user._id}</p>
                     </div>
                 }
             </div>
