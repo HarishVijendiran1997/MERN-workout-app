06acf3e8ddd6b154545b19051c7c9a2c5616c9a8
HarishVijendiran97 <Harishvijendiran@gmail.com>
2025-03-14 21:18:39 +0530

    fix: optimize app content with lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 // Importing necessary components
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { lazy, Suspense } from "react";
-import NavBar from "./components/NavBar.jsx"
-import { Slide, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { lazy, Suspense,useMemo } from "react";
 import { ThemeContextProvider } from "../contexts/ThemeContext.jsx";
 import { useThemeContext } from "../hooks/useThemeContext.jsx";
 import { useAuthContext } from "../hooks/useAuthContext.jsx";
-import ScrollToTop from "./components/ScrollToTop.jsx";
-import UserGuide from "./components/UserGuide.jsx";
-import Loader from "./components/Loader.jsx";
-import Profile from "./pages/Profile.jsx";
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 // Lazy-loaded components
 const Home = lazy(() => import("./pages/Home.jsx"));
 const Signup = lazy(() => import("./pages/Signup.jsx"));
@@ -19,16 +15,29 @@ const Test = lazy(() => import("./pages/Test.jsx"));
 const Plans = lazy(() => import("./pages/Plans.jsx"));
 const ForgotPassword = lazy(() => import("./pages/ForgotPassword.jsx"));
 const ResetPassword = lazy(() => import("./pages/ResetPassword.jsx"));
-
+const Profile = lazy(() => import("./pages/Profile.jsx"));
+const ScrollToTop = lazy(() => import("./components/ScrollToTop.jsx"));
+const NavBar = lazy(() => import("./components/NavBar.jsx"));
+const UserGuide = lazy(() => import("./components/UserGuide.jsx"));
+const Loader = lazy(() => import("./components/Loader.jsx"));
 
 function AppContent() {
   const { user } = useAuthContext()
   const { themeMode } = useThemeContext()
+  const MemoizedToastContainer = useMemo(() => (
+    <ToastContainer
+      limit={window.innerWidth <= 768 ? 0 : 3}
+      theme={themeMode}
+      position="bottom-right"
+      closeOnClick={true}
+      transition={Slide}
+      autoClose={3000}
+    />
+  ), [themeMode]);
   return (
     <div className="font-primary min-h-screen flex flex-col">
-      <BrowserRouter>
         {user && <UserGuide />}
-        <ToastContainer limit={window.innerWidth <= 768 ? 0 : 3} theme={themeMode} position="bottom-right" closeOnClick={true} transition={Slide} autoClose={3000} />
+        {MemoizedToastContainer}
         <NavBar className="relative" />
         <div className="flex flex-grow">
           <Suspense fallback={<Loader />}>
@@ -45,7 +54,6 @@ function AppContent() {
           </Suspense>
         </div>
         <ScrollToTop />
-      </BrowserRouter >
     </div >
   )
 }
@@ -53,7 +61,9 @@ function AppContent() {
 function App() {
   return (
     <ThemeContextProvider>
-      <AppContent />
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
     </ThemeContextProvider>
   );
 }
